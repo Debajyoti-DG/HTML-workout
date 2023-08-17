0026b8328077ef4c394178fc1d567fc58bb86bc8
Debajyoti <dattaguptadebajyoti@gmail.com>
2023-08-18 01:27:21 +0530

    Express backend commit

---
+++ Express/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+const port = 80; //by default port is 80 for sending and receiving unencrypted webpages
+
+// app.get('/', (req,res) =>{
+//     res.send("This is home page of app using Express");
+// });
+
+//To send status --> 200, 404,etc. (200 is default)
+app.get('/', (req,res) =>{
+    res.status(200).send("This is home page of app using Express");
+});
+
+//get request of about
+app.get('/about', (req,res) =>{
+    res.send("This is about page using Express");
+});
+//post request of about
+app.post('/about', (req,res) =>{
+    res.send("This is post request in about page using Express");
+});
+app.get('/contact', (req,res) =>{
+    res.send("This is contact page using Express");
+});
+app.get('/this', (req,res) =>{
+    res.status(404).send("This page is not available");
+});
+
+app.listen(port, ()=>{
+    console.log(`Application running succesfully at port : ${port}`);
+});
